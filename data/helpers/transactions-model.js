40af0b651dfaba0e9b8df4d73567dbc51d84589c
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-27 00:10:07 -0400

    Transactions-model file created. Helper functions for querying transactions table added.

---
+++ data/helpers/transactions-model.js
@@ -0,0 +1,48 @@
+const db = require("../dbConfig.js");
+
+module.exports = {
+  find,
+  findById,
+  findByUser,
+  create,
+  remove,
+  update
+};
+
+async function find() {
+  return db("transactions");
+}
+
+async function findById(id) {
+  return db("transactions")
+    .where({ id })
+    .first();
+}
+
+async function findByUser(user_id) {
+  return db("transactions")
+    .where({ "transactions.user_id": user_id })
+    .first();
+}
+
+async function create(transaction) {
+  const newTransaction = await db("transactions")
+    .insert(transaction)
+    .returning("*");
+  return newTransaction["0"];
+}
+
+async function remove(id) {
+  const [deletedTransaction] = await db("transactions")
+    .where({ id })
+    .del()
+    .returning("*");
+  return deletedTransaction;
+}
+
+async function update(updates, id) {
+  const [editedTransaction] = await db("transactions")
+    .where({ id })
+    .update(updates);
+  return editedTransaction;
+}

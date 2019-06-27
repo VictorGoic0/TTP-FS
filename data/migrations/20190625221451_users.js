af87c94a535161e55002c7aa56fc0014ae2f9ac1
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-27 00:04:10 -0400

    Migration for transactions table created.

--- data/migrations/20190625221451_users.js
+++ data/migrations/20190625221451_users.js
@@ -1,19 +1,33 @@
-
 exports.up = function(knex, Promise) {
-    return knex.schema
+  return knex.schema
     .createTable("users", table => {
       table.increments();
       table
         .string("email", 50)
         .notNullable()
         .unique();
-        table.string("name", 50)
-        .notNullable();
+      table.string("name", 50).notNullable();
       table.string("password", 100).notNullable();
+      table.float("balance").defaultTo(5000);
       table.timestamps(true, true);
     })
+    .createTable("transactions", table => {
+      table.increments();
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("users.id")
+        .notNullable()
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("symbol", 10).notNullable();
+      table.string("sector", 30).notNullable();
+      table.string("security_type", 30).notNullable();
+      table.float("price").notNullable();
+      table.integer("quantity").notNullable();
+    });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists("users")
+  return knex.schema.dropTableIfExists("users");
 };

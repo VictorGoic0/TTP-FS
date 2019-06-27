c92eab14ad7f2ca3fb6ee83576196757904544c7
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-27 01:09:07 -0400

    Passed in routes to main express server. Tested registration and login on Postman, confirmed to work.

--- api/server.js
+++ api/server.js
@@ -4,8 +4,13 @@ const cors = require("cors");
 
 const server = express();
 
+const authRouter = require("../routers/authRouter.js");
+const transactionRouter = require("../routers/transactionRouter.js");
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use("/api/auth", authRouter);
+server.use("/api/transactions", transactionRouter);
 
 module.exports = server;

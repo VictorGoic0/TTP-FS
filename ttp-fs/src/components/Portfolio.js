78f08094e8d677bff7df6274ebb8d69474e4dfcd
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-29 23:01:24 -0400

    Portfolio will make a request for user info if it isn't present, and then display the user's balance.

--- ttp-fs/src/components/Portfolio.js
+++ ttp-fs/src/components/Portfolio.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions";
 
 class Portfolio extends Component {
+  componentDidMount() {
+    if (!this.props.user.id) {
+      const user_id = localStorage.getItem("userID");
+      this.props.fetchUser(user_id);
+    }
+  }
   render() {
-    return <div />;
+    const { fetchingUser, user } = this.props;
+    const { id, balance } = user;
+    if (fetchingUser || !id) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div>
+          <h2>Balance:</h2>
+          <h2>${balance.toFixed(2)}</h2>
+          <form>
+            <input type="text" />
+            <input type="number" />
+          </form>
+        </div>
+      );
+    }
   }
 }
 
-export default Portfolio;
+const mapStateToProps = state => ({
+  user: state.user,
+  fetchingUser: state.fetchingUser,
+  error: state.error
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(Portfolio);

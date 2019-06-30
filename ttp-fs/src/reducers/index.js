3529bcbf6e6699eeea1a3d847b50ee75e98b97b9
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-29 21:53:58 -0400

    Reducer cases for signing up and signing in added.

--- ttp-fs/src/reducers/index.js
+++ ttp-fs/src/reducers/index.js
@@ -1,9 +1,63 @@
+import {
+  SIGN_UP,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAILURE,
+  SIGN_IN,
+  SIGN_IN_SUCCESS,
+  SIGN_IN_FAILURE
+} from "../actions";
+
 const initialState = {
+  signingIn: false,
+  signingUp: false,
+  signedIn: localStorage.getItem("token") ? true : false,
   error: null
 };
 
 const reducer = (state = initialState, action) => {
-  return null;
+  switch (action.type) {
+    case SIGN_UP:
+      return {
+        ...state,
+        signingUp: true,
+        error: null
+      };
+    case SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        signingUp: false,
+        signedIn: true
+      };
+    case SIGN_UP_FAILURE:
+      return {
+        ...state,
+        signingUp: false,
+        signedIn: false,
+        error: action.payload
+      };
+    case SIGN_IN:
+      return {
+        ...state,
+        signingIn: true,
+        error: null
+      };
+    case SIGN_IN_SUCCESS:
+      return {
+        ...state,
+        signingIn: false,
+        signedIn: true
+      };
+    case SIGN_IN_FAILURE:
+      return {
+        ...state,
+        signingIn: false,
+        signedIn: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;

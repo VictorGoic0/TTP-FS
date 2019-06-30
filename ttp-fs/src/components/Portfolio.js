2cbff8cbe584827fdd4bd0151773fe552aa28199
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-29 23:09:49 -0400

    Component logic for making a transaction built out.

--- ttp-fs/src/components/Portfolio.js
+++ ttp-fs/src/components/Portfolio.js
@@ -3,15 +3,41 @@ import { connect } from "react-redux";
 import { fetchUser } from "../actions";
 
 class Portfolio extends Component {
+  state = {
+    transaction: {
+      user_id: this.props.user.id
+        ? this.props.user.id
+        : localStorage.getItem("userID"),
+      symbol: "",
+      quantity: 0
+    }
+  };
   componentDidMount() {
     if (!this.props.user.id) {
       const user_id = localStorage.getItem("userID");
       this.props.fetchUser(user_id);
     }
   }
+
+  handleChanges = e => {
+    this.setState({
+      transaction: {
+        ...this.state.transaction,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  makeTransaction = (e, transacInfo) => {
+    e.preventDefault();
+    // Make transaction
+    console.log(transacInfo);
+  };
+
   render() {
     const { fetchingUser, user } = this.props;
     const { id, balance } = user;
+
     if (fetchingUser || !id) {
       return <h1>Loading...</h1>;
     } else {
@@ -19,9 +45,22 @@ class Portfolio extends Component {
         <div>
           <h2>Balance:</h2>
           <h2>${balance.toFixed(2)}</h2>
-          <form>
-            <input type="text" />
-            <input type="number" />
+          <form onSubmit={e => this.makeTransaction(e, this.state.transaction)}>
+            <input
+              type="text"
+              value={this.state.transaction.symbol}
+              name="symbol"
+              placeholder="Symbol"
+              onChange={this.handleChanges}
+            />
+            <input
+              type="number"
+              value={this.state.transaction.quantity}
+              name="quantity"
+              placeeholder="Quantity"
+              onChange={this.handleChanges}
+            />
+            <button type="submit">BUY</button>
           </form>
         </div>
       );

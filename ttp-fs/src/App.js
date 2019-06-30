1e9836af94f4c06e9acc271c9814efa66355522a
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-29 20:24:34 -0400

    App.js boilerplate for routers added.

--- ttp-fs/src/App.js
+++ ttp-fs/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Portfolio from "./components/Portfolio";
+import Transactions from "./components/Transactions";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Title</h1>
+    <div>
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Route exact path="/" component={Portfolio} />
+      <Route path="/transactions" component={Transactions} />
     </div>
   );
 }

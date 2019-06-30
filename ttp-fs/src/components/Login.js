8eb3c09ab3adb51cd70a59f624da17f185d9b2b7
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-29 20:42:23 -0400

    Boilerplate login form class component built out.

--- ttp-fs/src/components/Login.js
+++ ttp-fs/src/components/Login.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  state = {
+    user: {
+      email: "",
+      password: ""
+    }
+  };
+
+  handleChanges = e => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
   render() {
-    return <div />;
+    return (
+      <div className="register">
+        <h1>Sign In</h1>
+        <form>
+          <input
+            type="text"
+            value={this.state.user.email}
+            name="email"
+            onChange={this.handleChanges}
+            placeholder="email"
+          />
+          <input
+            type="password"
+            value={this.state.user.password}
+            name="password"
+            onChange={this.handleChanges}
+            placeholder="Password"
+          />
+          <button>Sign In</button>
+        </form>
+        <h3>
+          Don't Have An Account? <span>Sign Up</span>
+        </h3>
+      </div>
+    );
   }
 }
 

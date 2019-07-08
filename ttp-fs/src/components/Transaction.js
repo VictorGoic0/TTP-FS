692d3c252510c31559d0df49be42a777411d2300
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-07 23:56:28 -0400

    Adjusted Transaction component to render BUY or SELL depending on transaction type.

--- ttp-fs/src/components/Transaction.js
+++ ttp-fs/src/components/Transaction.js
@@ -1,10 +1,14 @@
 import React, { memo } from "react";
 
 const Transaction = props => {
-  const { symbol, quantity, price } = props.transaction;
+  const { symbol, quantity, price, transaction_type } = props.transaction;
   return (
     <div className="transaction">
-      <h3>BUY ({symbol})</h3>
+      {transaction_type === "BUY" ? (
+        <h3>BUY ({symbol})</h3>
+      ) : (
+        <h3>SELL ({symbol})</h3>
+      )}
       <h3>
         {quantity} shares @ {price.toFixed(2)}
       </h3>

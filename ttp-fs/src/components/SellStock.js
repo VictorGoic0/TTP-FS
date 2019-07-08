9dcd97927b42a1c02f18a36655e98f9615678e08
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-07 23:12:37 -0400

    Copy pasted PurchaseStock code into a new component, SellStock. Added fetchPrices method. Added some fetches to CDM for price data and stockList. Rewrote makeTransaction for a sell transaction.

---
+++ ttp-fs/src/components/SellStock.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  fetchUser,
+  getTransactions,
+  fetchPrices,
+  makeTransaction
+} from "../actions";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Spinner from "./Spinner";
+
+class PurchaseStock extends Component {
+  state = {
+    transaction: {
+      user_id: this.props.user.id
+        ? this.props.user.id
+        : localStorage.getItem("userID"),
+      symbol: "",
+      quantity: ""
+    }
+  };
+
+  componentDidMount() {
+    const user_id = this.props.user.id
+      ? this.props.user.id
+      : localStorage.getItem("userID");
+    if (!this.props.user.id) {
+      this.props.fetchUser(user_id);
+    }
+    if (this.props.stockList.length === 0) {
+      this.props.getTransactions(user_id);
+    }
+    if (
+      this.props.stockList.length > 0 &&
+      Object.keys(this.props.prices).length === 0
+    ) {
+      this.fetchPrices();
+    }
+  }
+
+  handleChanges = e => {
+    this.setState({
+      transaction: {
+        ...this.state.transaction,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  fetchPrices = () => {
+    const symbols = this.props.stockList.map(stock => stock.symbol);
+    this.props.fetchPrices(symbols);
+  };
+
+  makeTransaction = e => {
+    e.preventDefault();
+    // Make request to IEX API and check price. If quantity * price < user balance, then make the transaction.
+    const { id } = this.props.user; // User funds
+    const { quantity, symbol } = this.state.transaction;
+    if (symbol in this.props.prices) {
+      const stock = this.props.stockList.find(stock => {
+        return stock.symbol === symbol.toUpperCase();
+      });
+      if (stock.quantity >= quantity) {
+        // Sell
+      } else {
+        alert("You are trying to sell more of this stock than you own.");
+      }
+    } else {
+      alert("You do not own this stock.");
+    }
+  };
+
+  render() {
+    const { user, fetchingUser } = this.props;
+    const { balance, id } = user;
+    if (fetchingUser || !id) {
+      return <Spinner />;
+    } else {
+      return (
+        <div className="purchase">
+          <h1>Balance - ${balance.toFixed(2)}</h1>
+          <form className="purchase-form" onSubmit={this.makeTransaction}>
+            <FormControl>
+              <InputLabel className="label">Symbol</InputLabel>
+              <Input
+                type="text"
+                value={this.state.transaction.symbol}
+                name="symbol"
+                onChange={this.handleChanges}
+                fullWidth
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel className="label">Quantity</InputLabel>
+              <Input
+                type="number"
+                value={this.state.transaction.quantity}
+                name="quantity"
+                onChange={this.handleChanges}
+                fullWidth
+              />
+            </FormControl>
+            <Button type="submit" variant="contained">
+              SELL
+            </Button>
+          </form>
+        </div>
+      );
+    }
+  }
+}
+
+const mapStateToProps = state => ({
+  user: state.user,
+  stockList: state.stockList,
+  prices: state.prices,
+  fetchingUser: state.fetchingUser,
+  error: state.error
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchUser, makeTransaction, getTransactions, fetchPrices }
+)(PurchaseStock);
